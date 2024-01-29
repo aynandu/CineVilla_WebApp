06d84c59d3ef77290589b83c907485e4dfa41f6d
Nandu AY <nandu.a.y@hotmail.com>
2024-01-29 18:22:44 +0530

    updated app/models.py ,app/admin.py,settings.py : added app

--- cine_project/cine_app/admin.py
+++ cine_project/cine_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import MovieDetails,Users,Category
 # Register your models here.
+admin.site.register(Users)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    prepopulated_fields={'slug':('name',)}
+admin.site.register(Category,CategoryAdmin)
+
+class MovieDetailsAdmin(admin.ModelAdmin):
+    list_display=['title','description','release_date','actors','category']
+    prepopulated_fields={'slug':('title',)}
+admin.site.register(MovieDetails,MovieDetailsAdmin)
--- cine_project/cine_app/models.py
+++ cine_project/cine_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username=models.CharField(max_length=10,unique=True)
+    first_Name=models.CharField(max_length=20,unique=True)
+    last_Name=models.CharField(max_length=20)
+    email=models.EmailField(max_length=70)
+    password=models.CharField(max_length=20)
+
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    slug=models.SlugField(max_length=150,unique=True)
+    class Meta:
+        ordering=('name',)
+        verbose_name='category'
+        verbose_name_plural='categories'
+
+class MovieDetails(models.Model):
+    title=models.CharField(max_length=80)
+    slug=models.SlugField(max_length=150,unique=True)
+    poster=models.ImageField(upload_to='moviedetails',blank=True)
+    description=models.CharField(max_length=250)
+    release_date=models.DateField()
+    actors=models.CharField(max_length=250)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    trailer_Link=models.CharField(max_length=250,blank=True)
